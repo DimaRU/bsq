16519b3a4759c2398a10fbab56bb354601f6f051
ssantiag <ssantiag@student.21-school.ru>
2021-02-06 16:57:28 +0300

    Initial Commit

---
+++ bsq-xcode/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  bsq-xcode
+//
+//  Created by Sheri Santiago on 2/6/21.
+//  Copyright © 2021 Sheri Santiago. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
